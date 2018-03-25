5e9ccfc7ac29def306f0c89b2511a29b7e2ce5f9
redshiftzero <jen@freedom.press>
2018-03-25 15:38:31 -0700

    Move padding tests into their own test file

---
+++ padding_test.go
@@ -0,0 +1,25 @@
+package gocryptopals_test
+
+import (
+"testing"
+"github.com/redshiftzero/gocryptopals"
+)
+
+var pkcs7paddingtests = []struct {
+	plaintext string
+	blocksize int
+	out       string
+}{
+	{"YELLOW SUBMARINE", 16, "YELLOW SUBMARINE"},
+	{"YELLOW SUBMARINE", 10, "YELLOW SUBMARINE\x04\x04\x04\x04"},
+	{"YELLOW SUBMARINE", 20, "YELLOW SUBMARINE\x04\x04\x04\x04"},
+}
+
+func TestPKCS7Pad(t *testing.T) {
+	for _, tt := range pkcs7paddingtests {
+		s := gocryptopals.PKCS7Pad(tt.plaintext, tt.blocksize)
+		if s != tt.out {
+			t.Errorf("PKCS7Pad(%q, %q) => %q, want %q", tt.plaintext, tt.blocksize, s, tt.out)
+		}
+	}
+}
--- second_set_test.go
+++ second_set_test.go
@@ -9,25 +9,6 @@ import (
 	"github.com/redshiftzero/gocryptopals"
 )
 
-var pkcs7paddingtests = []struct {
-	plaintext string
-	blocksize int
-	out       string
-}{
-	{"YELLOW SUBMARINE", 16, "YELLOW SUBMARINE"},
-	{"YELLOW SUBMARINE", 10, "YELLOW SUBMARINE\x04\x04\x04\x04"},
-	{"YELLOW SUBMARINE", 20, "YELLOW SUBMARINE\x04\x04\x04\x04"},
-}
-
-func TestPKCS7Pad(t *testing.T) {
-	for _, tt := range pkcs7paddingtests {
-		s := gocryptopals.PKCS7Pad(tt.plaintext, tt.blocksize)
-		if s != tt.out {
-			t.Errorf("PKCS7Pad(%q, %q) => %q, want %q", tt.plaintext, tt.blocksize, s, tt.out)
-		}
-	}
-}
-
 func TestAESInCBCMode(t *testing.T) {
 	content, err := ioutil.ReadFile("challengefiles/10.txt")
 	if err != nil {
